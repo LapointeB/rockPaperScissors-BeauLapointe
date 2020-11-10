06468d0d2c00f32315753bc63cd7c3003b7d4625
LapointeB <LapointeB@T29685.niaccit.niacc.edu>
2020-11-10 09:34:12 -0600

    I did the JavaScript code

--- public_html/rockPaperScissors.js
+++ public_html/rockPaperScissors.js
@@ -3,7 +3,59 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+// Listeners for what button is pressed
+document.getElementById("rockButton").addEventListener("click",choseRock);
+document.getElementById("paperButton").addEventListener("click",chosePaper);
+document.getElementById("scissorsButton").addEventListener("click",choseScissors);
 
+// Function to find the computer's choice
+function randomChoice() {
+    let compChoice = Math.ceil(Math.random() * 3);
+    if (compChoice === 1) {
+        return "rock";
+    } else if (compChoice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
 
+// Function if rock is selected
+function choseRock() {
+    let computerChoice = randomChoice();
+    alert(computerChoice);
+    if (computerChoice === "rock") {
+        alert("Draw! Play again!");
+    } else if (computerChoice === "paper") {
+        alert("You lose!");
+    } else {
+        alert("You win!");
+    }
+}
 
+// Function if paper is selected
+function chosePaper() {
+     let computerChoice = randomChoice();
+    alert(computerChoice);
+    if (computerChoice === "paper") {
+        alert("Draw! Play again!");
+    } else if (computerChoice === "scissors") {
+        alert("You lose!");
+    } else {
+        alert("You win!");
+    }   
+}
+
+// Function if scissors is selected
+function choseScissors() {
+    let computerChoice = randomChoice();
+    alert(computerChoice);
+    if (computerChoice === "scissors") {
+        alert("Draw! Play again!");
+    } else if (computerChoice === "rock") {
+        alert("You lose!");
+    } else {
+        alert("You win!");
+    }    
+}
 
